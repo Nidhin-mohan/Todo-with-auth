e0e081037a54e773d24d51cc9242d03426726c16
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-03-18 08:25:48 +0530

    Added  userSchema to models

---
+++ models/userSchema.js
@@ -0,0 +1,58 @@
+// Import required packages
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+
+// Create user schema
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: [6, "password must be atleast 6 character"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Add more methods to the schema
+userSchema.methods = {
+  // Compare password
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+
+  // Generate JWT token
+  getJwtToken: function () {
+    return JWT.sign(
+      {
+        _id: this._id,
+        name: this.name,
+      },
+      {
+        expiresIn: process.env.JWT_EXPIRY,
+      }
+    );
+  },
+};
+
+// Export the user model
+module.exports = mongoose.model("User", userSchema);
