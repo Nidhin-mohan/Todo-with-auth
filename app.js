aa0197f302e233b9a119a446e82daedf86fbce2e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-03-19 22:32:44 +0530

    added yml file for auth

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const cors = require("cors");
 const app = express();
 
 
+//for swagger documentation
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
